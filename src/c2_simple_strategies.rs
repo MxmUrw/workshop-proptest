4117d2ce06c0aa9022c33c0f25b5a89cd1a7a580
Maxim Urschumzew <maxu@chainflip.io>
2025-06-12 11:52:05 +0200

    feat: updated chapter 2.

--- src/c2_simple_strategies.rs
+++ src/c2_simple_strategies.rs
@@ -1,20 +1,25 @@
 use proptest::{collection::vec, prelude::*};
 
 
-fn sum_all_below(below: u32, values: Vec<u32>) -> u32 {
+// This function has to some all entries of `values` that are below `below`,
+// but there's a bug here.
+fn sum_all_below(mut below: u32, values: Vec<u32>) -> u32 {
     let mut result = 0;
     for x in values {
-        if x <= below {
+        if result > 4000 {
+            below += 1;
+        }
+        if x < below {
             result += x;
         }
     }
     return result;
 }
 
-
 proptest! {
+    // Proptest is going to find inputs that trigger the bug.
     #[test]
-    fn test_sum_all(below in any::<u32>(), values in vec(any::<u32>(), 0..100)) {
+    fn test_sum_all(below in 0..1000u32, values in vec(0..1000u32, 0..20)) {
         assert_eq!(
             sum_all_below(below, values.clone()),
             values.iter().filter(|x| **x < below).sum()
